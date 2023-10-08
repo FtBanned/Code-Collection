55b554a63c6f73686a2881b8834235234ab40e88
FtBanned <sirzadlimehdi48@gmail.com>
2023-10-08 20:42:27 +0400

    LoginForm #1

---
+++ login.js
@@ -0,0 +1,189 @@
+const translations = {
+  es: {
+    name: "Nombre",
+    email: "Correo Electrónico",
+    password: "Contraseña",
+    signUp: "Registrarse",
+    signIn: "Iniciar Sesión",
+    orUseEmail: "o utiliza tu correo electrónico para registrarte",
+    forgotPassword: "¿Olvidaste tu contraseña?",
+    welcomeBack: "¡Bienvenido de nuevo!",
+    keepConnected: "Para mantener la conexión con nosotros, inicia sesión con tu información personal",
+    helloFriend: "¡Hola, Amigo!",
+    enterDetails: "Ingresa tus datos personales y comienza el viaje con nosotros",
+  },
+  ar: {
+    name: "الاسم",
+    email: "البريد الإلكتروني",
+    password: "كلمة المرور",
+    signUp: "سجل",
+    signIn: "تسجيل الدخول",
+    orUseEmail: "أو استخدم بريدك الإلكتروني للتسجيل",
+    forgotPassword: "نسيت كلمة المرور؟",
+    welcomeBack: "مرحبًا مرة أخرى!",
+    keepConnected: "للبقاء متصلًا معنا، يرجى تسجيل الدخول باستخدام معلوماتك الشخصية",
+    helloFriend: "مرحبًا، صديق!",
+    enterDetails: "أدخل تفاصيلك الشخصية وابدأ الرحلة معنا",
+  },
+  fr: {
+    name: "Nom",
+    email: "Email",
+    password: "Mot de passe",
+    signUp: "S'inscrire",
+    signIn: "Se connecter",
+    orUseEmail: "ou utilisez votre e-mail pour vous inscrire",
+    forgotPassword: "Mot de passe oublié ?",
+    welcomeBack: "Bienvenue de retour !",
+    keepConnected: "Pour rester connecté avec nous, veuillez vous connecter avec vos informations personnelles",
+    helloFriend: "Salut, l'ami !",
+    enterDetails: "Entrez vos informations personnelles et commencez le voyage avec nous",
+  },
+  ru: {
+    name: "Имя",
+    email: "Электронная почта",
+    password: "Пароль",
+    signUp: "Регистрация",
+    signIn: "Войти",
+    orUseEmail: "или используйте свою электронную почту для регистрации",
+    forgotPassword: "Забыли пароль?",
+    welcomeBack: "С возвращением!",
+    keepConnected: "Для поддержания связи с нами, пожалуйста, войдите с вашей персональной информацией",
+    helloFriend: "Привет, Друг!",
+    enterDetails: "Введите свои персональные данные и начните путешествие с нами",
+  },
+  de: {
+    name: "Name",
+    email: "E-Mail",
+    password: "Passwort",
+    signUp: "Registrieren",
+    signIn: "Anmelden",
+    orUseEmail: "oder verwenden Sie Ihre E-Mail-Adresse zur Registrierung",
+    forgotPassword: "Passwort vergessen?",
+    welcomeBack: "Willkommen zurück!",
+    keepConnected: "Um mit uns in Verbindung zu bleiben, melden Sie sich bitte mit Ihren persönlichen Daten an",
+    helloFriend: "Hallo, Freund!",
+    enterDetails: "Geben Sie Ihre persönlichen Daten ein und beginnen Sie die Reise mit uns",
+  },
+  en: {
+    name: "Name",
+    email: "Email",
+    password: "Password",
+    signUp: "Sign Up",
+    signIn: "Sign In",
+    orUseEmail: "or use your email for registration",
+    forgotPassword: "Forgot your password?",
+    welcomeBack: "Welcome Back!",
+    keepConnected: "To keep connected with us please login with your personal info",
+    helloFriend: "Hello, Friend!",
+    enterDetails: "Enter your personal details and start journey with us",
+  },
+  tr: {
+    name: "Ad",
+    email: "E-posta",
+    password: "Parola",
+    signUp: "Kaydol",
+    signIn: "Giriş Yap",
+    orUseEmail: "veya kayıt için e-postanızı kullanın",
+    forgotPassword: "Parolanızı mı unuttunuz?",
+    welcomeBack: "Tekrar Hoş Geldiniz!",
+    keepConnected: "Bizimle bağlantı kurmak için lütfen kişisel bilgilerinizle giriş yapın",
+    helloFriend: "Merhaba, Arkadaş!",
+    enterDetails: "Kişisel bilgilerinizi girin ve bizimle yolculuğa başlayın",
+  },
+  az: {
+    name: "Ad",
+    email: "E-poçt",
+    password: "Şifrə",
+    signUp: "Qeydiyyat",
+    signIn: "Giriş et",
+    orUseEmail: "veya qeydiyyat üçün e-poçtunuzu istifadə edin",
+    forgotPassword: "Şifrənizi unutmusunuz?",
+    welcomeBack: "Yenidən xoş gəldiniz!",
+    keepConnected: "Bizimlə əlaqə saxlamaq üçün lütfən şəxsi məlumatlarınızla daxil olun",
+    helloFriend: "Salam, Dost!",
+    enterDetails: "Şəxsi məlumatlarınızı daxil edin və bizimlə səyahətinizi başlayın",
+  },
+  ja: {
+    name: "名前",
+    email: "メールアドレス",
+    password: "パスワード",
+    signUp: "新規登録",
+    signIn: "ログイン",
+    orUseEmail: "またはメールアドレスを使用して登録",
+    forgotPassword: "パスワードをお忘れですか？",
+    welcomeBack: "お帰りなさい！",
+    keepConnected: "私たちと連絡を保つために、個人情報を使用してログインしてください",
+    helloFriend: "こんにちは、友達！",
+    enterDetails: "個人情報を入力して、私たちとの旅行を始めましょう",
+  },
+  vi: {
+    name: "Tên",
+    email: "Email",
+    password: "Mật khẩu",
+    signUp: "Đăng ký",
+    signIn: "Đăng nhập",
+    orUseEmail: "hoặc sử dụng email của bạn để đăng ký",
+    forgotPassword: "Quên mật khẩu?",
+    welcomeBack: "Chào mừng trở lại!",
+    keepConnected: "Để duy trì kết nối với chúng tôi, vui lòng đăng nhập bằng thông tin cá nhân của bạn",
+    helloFriend: "Xin chào, bạn!",
+    enterDetails: "Nhập thông tin cá nhân và bắt đầu hành trình với chúng tôi",
+  },
+  hi: { 
+    name: "नाम",
+    email: "ईमेल",
+    password: "पासवर्ड",
+    signUp: "साइन अप करें",
+    signIn: "साइन इन करें",
+    orUseEmail: "या अपने ईमेल का उपयोग करके पंजीकरण करें",
+    forgotPassword: "क्या आपने अपना पासवर्ड भूल गए हैं?",
+    welcomeBack: "वापसी पर स्वागत है!",
+    keepConnected: "हमारे साथ जुड़े रहने के लिए कृपया अपनी व्यक्तिगत जानकारी के साथ लॉगिन करें",
+    helloFriend: "नमस्कार, दोस्त!",
+    enterDetails: "अपना व्यक्तिगत विवरण दर्ज करें और हमारे साथ यात्रा शुरू करें",
+  },
+};
+  
+  const signUpButton = document.getElementById('signUp');
+  const signInButton = document.getElementById('signIn');
+  const container = document.getElementById('container');
+  
+  const languageSelect = document.getElementById('language-select');
+  languageSelect.addEventListener('change', () => {
+    const selectedLanguage = languageSelect.value;
+    translateUI(selectedLanguage);
+  });
+  
+  function translateUI(language) {
+    const elements = document.querySelectorAll('[data-translate]');
+    elements.forEach(element => {
+      const key = element.getAttribute('data-translate');
+      if (translations[language] && translations[language][key]) {
+        if (element.tagName === 'INPUT' && element.getAttribute('type') === 'text') {
+          // Placeholder çevirisi için input alanını seçip placeholder'ı güncelle
+          element.placeholder = translations[language][key];
+        } else if (element.tagName === 'INPUT' && element.getAttribute('type') === 'email') {
+          // Email input alanı için placeholder'ı güncelle
+          element.placeholder = translations[language][key];
+        } else if (element.tagName === 'INPUT' && element.getAttribute('type') === 'password') {
+          // Password input alanı için placeholder'ı güncelle
+          element.placeholder = translations[language][key];
+        } else {
+          element.textContent = translations[language][key];
+        }
+      }
+    });
+  }
+  signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+    translateUI(languageSelect.value);
+  });
+  
+  signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+    translateUI(languageSelect.value);
+  });
+  
+  
+  translateUI('az');
+  
